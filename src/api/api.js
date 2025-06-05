5163c1d32fd04e36fa8710dd20c7de96d1e2d89f
Ariprakash <ariprakashapk@gmail.com>
2025-06-05 12:43:39 +0530

    feat: api authentication with token via cookies

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,26 @@
 import axios from "axios";
 
+export function getCookieByName(cookiename) {
+  // Get name followed by anything except a semicolon
+  const cookiestring = RegExp(`${cookiename}=[^;]+`).exec(document.cookie);
+  // Return everything after the equal sign, or an empty string if the cookie name not found
+  return decodeURIComponent(
+    cookiestring ? cookiestring?.toString().replace(/^[^=]+./, "") : ""
+  );
+}
+export const deleteCookie = (name) => {
+  document.cookie = name + "=";
+};
+
+export function handleApiError(error) {
+  console.log(error);
+}
+// To delete a specific cookie by name, call the function with the cookie name
+
 export const instance = () => {
   return axios.create({
     baseURL: import.meta.env.VITE_APP_BASE_API_URL,
-    headers: {},
+    headers: { Authorization: `Bearer ${getCookieByName("token1")}` },
+    // mode: "no cors"
   });
 };
