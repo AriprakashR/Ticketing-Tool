e236cd5a869078cdce21e03e697f50e6b45fa5d9
Ariprakash <ariprakashapk@gmail.com>
2025-06-23 19:38:58 +0530

    style: login page

--- src/components/Auth/LoginForm.jsx
+++ src/components/Auth/LoginForm.jsx
@@ -1,15 +1,20 @@
-import { Box, Button, TextField, Typography } from "@mui/material";
+import { Box, Button, TextField, Typography, InputAdornment, IconButton, useTheme } from "@mui/material";
+import { Icon } from "@iconify/react";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
 import { postEmployeeLogin } from "../../api/auth-service";
 import { toast } from "../../utils/toastService";
 
 const LoginForm = () => {
+  const theme = useTheme();
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
     username: "",
     password: "",
   });
+  const [showPassword, setShowPassword] = useState(false);
+
+  const iconColor = theme.palette.mode === "dark" ? "#919EAB" : "#637381";
 
   const handleChange = (e) => {
     setFormData({
@@ -18,14 +23,20 @@ const LoginForm = () => {
     });
   };
 
+  const handleClickShowPassword = () => {
+    setShowPassword((prev) => !prev);
+  };
+
+  const handleMouseDownPassword = (e) => {
+    e.preventDefault(); // prevent focus loss
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const response = await postEmployeeLogin(formData);
       if (response?.access_token) {
-        document.cookie = `token1=${encodeURIComponent(
-          response.access_token
-        )}; path=/;`;
+        document.cookie = `token1=${encodeURIComponent(response.access_token)}; path=/;`;
         toast.success("Login successful");
         navigate("/dashboard");
       } else {
@@ -41,43 +52,88 @@ const LoginForm = () => {
   }, []);
 
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      minHeight="100vh"
-    >
-      <Box maxWidth={420}>
-        <Typography variant="h5" align="center" gutterBottom>
-          Login
+    <Box display="flex" minHeight="100vh">
+      <Box
+        sx={{
+          width: {
+            md: 480,
+          },
+          bgcolor: theme.palette.mode === "dark" ? "#1D242B" : "#FAFAFA",
+          paddingTop: "72px",
+          paddingBottom: "24px",
+          paddingX: "24px",
+          display: {
+            xs: "none",
+            md: "flex",
+          },
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          gap: {
+            md: `calc(8 * var(--spacing))`, // your media query equivalent
+          },
+        }}
+      >
+        <Typography variant="h3" fontWeight={700} fontSize={30} align="center" gutterBottom>
+          Hi, Welcome back
         </Typography>
-        <form onSubmit={handleSubmit}>
-          <TextField
-            fullWidth
-            label="User ID"
-            name="username"
-            margin="normal"
-            value={formData.username}
-            onChange={handleChange}
-          />
-          <TextField
-            fullWidth
-            label="Password"
-            name="password"
-            type="password"
-            margin="normal"
-            value={formData.password}
-            onChange={handleChange}
-          />
-          <Button
-            fullWidth
-            variant="contained"
-            type="submit"
-            sx={{ mt: 2, p: 1.5 }}
-          >
-            Sign In
-          </Button>
-        </form>
+        <Typography variant="body1" color="#919EAB" align="center" gutterBottom>
+          Manage and Resolve Support Tickets Seamlessly.
+        </Typography>
+        <Box
+          component="img"
+          src="/src/assets/login-illustration.png" // Replace with your image path
+          alt="Login Illustration"
+          sx={{ width: "100%", maxWidth: 400, mt: 4 }}
+        />
+      </Box>
+
+      <Box display="flex" justifyContent="center" flexGrow={1} alignItems="center" p={4}>
+        <Box maxWidth={440} width="100%">
+          <Box>
+            <Typography variant="h4" fontWeight={700} fontSize={30} gutterBottom>
+              Login
+            </Typography>
+            <Typography variant="body2" color="#919EAB" gutterBottom>
+              Please log in with your credentials to access the Ticketing System.
+            </Typography>
+          </Box>
+          <form onSubmit={handleSubmit}>
+            <TextField
+              fullWidth
+              label="User ID"
+              name="username"
+              margin="normal"
+              value={formData.username}
+              onChange={handleChange}
+            />
+            <TextField
+              fullWidth
+              label="Password"
+              name="password"
+              type={showPassword ? "text" : "password"}
+              margin="normal"
+              value={formData.password}
+              onChange={handleChange}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton onClick={handleClickShowPassword} onMouseDown={handleMouseDownPassword} edge="end">
+                      <Icon
+                        icon={showPassword ? "solar:eye-bold" : "solar:eye-closed-bold"}
+                        color={iconColor}
+                        fontSize={20}
+                      />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <Button fullWidth variant="contained" type="submit" sx={{ mt: 4, p: 1.5, fontWeight: 700 }}>
+              Login now
+            </Button>
+          </form>
+        </Box>
       </Box>
     </Box>
   );
--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -1,23 +1,17 @@
 import { Outlet } from "react-router";
-import Header from "../components/Layouts/Header";
-import { useState, useEffect } from "react";
-import { ThemeProvider, CssBaseline, Box } from "@mui/material";
+import { ThemeProvider, CssBaseline, AppBar, Toolbar, Typography, IconButton, Box } from "@mui/material";
 import { lightTheme, darkTheme } from "../theme";
+import { alpha } from "@mui/material/styles";
+import { Icon } from "@iconify/react";
+import { useState, useEffect } from "react";
 
-const getSystemPreference = () =>
-  window.matchMedia("(prefers-color-scheme: dark)").matches;
+const getSystemPreference = () => window.matchMedia("(prefers-color-scheme: dark)").matches;
 
 const PublicLayout = () => {
   const [isDarkMode, setIsDarkMode] = useState(() => {
     const saved = localStorage.getItem("theme-mode");
-    return saved === "dark"
-      ? true
-      : saved === "light"
-      ? false
-      : getSystemPreference();
+    return saved === "dark" ? true : saved === "light" ? false : getSystemPreference();
   });
-
-  const [collapsed, setCollapsed] = useState(true);
 
   const toggleDarkMode = () => {
     setIsDarkMode((prev) => {
@@ -40,11 +34,51 @@ const PublicLayout = () => {
   return (
     <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
       <CssBaseline />
-      <Header
-        isDarkMode={isDarkMode}
-        toggleDarkMode={toggleDarkMode}
-        collapsed={collapsed}
-      />
+      <AppBar
+        variant="outlined"
+        position="fixed"
+        sx={{
+          minHeight: "70px",
+          zIndex: 1000,
+          backdropFilter: "none",
+          backgroundColor: "transparent",
+          boxShadow: "none",
+          border: "none",
+        }}
+      >
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+          <Box
+            component="img"
+            src="/src/assets/ct-logo-bg-rmv.png"
+            alt="Company Logo"
+            sx={{
+              height: 40,
+              width: 50,
+              ml: -0.7,
+              mt: 2,
+              mb: 1.8,
+            }}
+          />
+          <Typography
+            sx={{
+              color: "text.primary",
+              marginTop: 0.5,
+              marginLeft: 1.5,
+              marginBottom: 0.5,
+              fontWeight: 600,
+            }}
+          >
+            Cloute Technologies
+          </Typography>
+          <IconButton onClick={toggleDarkMode} sx={{ marginLeft: "auto" }}>
+            {isDarkMode ? (
+              <Icon icon="solar:sun-2-bold-duotone" color="#919EAB" />
+            ) : (
+              <Icon icon="solar:moon-stars-bold-duotone" color="#637381" />
+            )}
+          </IconButton>
+        </Toolbar>
+      </AppBar>
       <Outlet />
     </ThemeProvider>
   );
