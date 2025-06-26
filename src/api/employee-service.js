038cbd9a73964707b74ca128cda0b9fc417dbb3a
Ariprakash <ariprakashapk@gmail.com>
2025-06-26 13:19:18 +0530

    feat: pincode api and payload chnages in employee form.

--- src/api/employee-service.js
+++ src/api/employee-service.js
@@ -11,3 +11,14 @@ export const getEmployeeDetailsList = async () => {
     toast.error(error.response?.data?.msg || "Failed to get employee list");
   }
 };
+
+export const getBranchListByEmployeeDesignationId = async (empDesgId) => {
+  try {
+    const response = await instance.get(`empDesignation/getListOfBranchDetailsByEmpDesgId/${empDesgId}`);
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.log(error.response?.data?.msg);
+    toast.error(error.response?.data?.msg || "Failed to get branch list");
+  }
+};
--- src/components/Employee/EmployeeForm.jsx
+++ src/components/Employee/EmployeeForm.jsx
@@ -15,8 +15,9 @@ import { useState } from "react";
 
 const EmployeeForm = () => {
   const [formData, setFormData] = useState({
-    compId: "",
     empDesgId: "",
+    regionalId: "",
+    branchId: "",
     empName: "",
     empPhNo: "",
     add1: "",
@@ -28,9 +29,33 @@ const EmployeeForm = () => {
     locId: "",
   });
 
+  const [errors, setErrors] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [cityOptions, setCityOptions] = useState([]);
+
+  const fetchPincodeDetails = async (pincode, type) => {
+    try {
+      const response = await fetch(`https://api.postalpincode.in/pincode/${pincode}`);
+      const data = await response.json();
+
+      if (data?.[0]?.Status === "Success") {
+        const postOffices = data[0].PostOffice;
+        const cities = [...new Set(postOffices.flatMap(({ Name, District }) => [Name, District]))];
+        const state = postOffices[0]?.State || "";
+
+        setCityOptions(cities);
+        setFormData((prev) => ({ ...prev, state: state, city: "" }));
+      }
+    } catch (error) {
+      console.error("Error fetching pincode:", error);
+      toast.error("Failed to fetch pincode details.");
+    }
+  };
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prev) => ({ ...prev, [name]: value }));
+    if (name === "pincode" && value.length === 6) fetchPincodeDetails(value, "city");
   };
 
   const handleSubmit = (e) => {
@@ -44,6 +69,16 @@ const EmployeeForm = () => {
     { id: "Emp1002", name: "Branch Manager" },
     { id: "Emp1003", name: "Service Engineer" },
     { id: "Emp1004", name: "Admin" },
+  ];
+
+  const regionOptions = [
+    { id: "1", name: "Tamilnadu" },
+    { id: "2", name: "Karnataka" },
+  ];
+
+  const branchOptions = [
+    { id: "1", name: "Chennai" },
+    { id: "2", name: "Banglore" },
   ];
 
   const locationOptions = [
@@ -92,44 +127,72 @@ const EmployeeForm = () => {
               />
             </Grid>
             <Grid size={{ xs: 12, sm: 6 }}>
-              <FormControl fullWidth>
-                <InputLabel id="designation-select-label">
-                  Designation
-                </InputLabel>
-                <Select
-                  labelId="designation-select-label"
-                  name="empDesgId"
-                  label="Select Designation"
-                  fullWidth
-                  value={formData.empDesgId}
-                  onChange={handleChange}
-                >
-                  {designationOptions.map((desgn) => (
-                    <MenuItem key={desgn.id} value={desgn.id}>
-                      {desgn.name}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
+              <TextField
+                fullWidth
+                select
+                id="designation-select-label"
+                name="empDesgId"
+                label="Select Designation"
+                value={formData.empDesgId}
+                onChange={handleChange}
+              >
+                {designationOptions.map((desgn) => (
+                  <MenuItem key={desgn.id} value={desgn.id}>
+                    {desgn.name}
+                  </MenuItem>
+                ))}
+              </TextField>
             </Grid>
             <Grid size={{ xs: 12, sm: 6 }}>
-              <FormControl fullWidth>
-                <InputLabel id="location-select-label">Location</InputLabel>
-                <Select
-                  labelId="location-select-label"
-                  name="locId"
-                  label="Select Location"
-                  fullWidth
-                  value={formData.locId}
-                  onChange={handleChange}
-                >
-                  {locationOptions.map((loc) => (
-                    <MenuItem key={loc.id} value={loc.id}>
-                      {loc.name}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
+              <TextField
+                fullWidth
+                select
+                id="region-select-label"
+                name="regionalId"
+                label="Select Region"
+                value={formData.regionalId}
+                onChange={handleChange}
+              >
+                {regionOptions.map((region) => (
+                  <MenuItem key={region.id} value={region.id}>
+                    {region.name}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </Grid>
+            <Grid size={{ xs: 12, sm: 6 }}>
+              <TextField
+                fullWidth
+                select
+                id="branch-select-label"
+                name="branchId"
+                label="Select Branch"
+                value={formData.branchId}
+                onChange={handleChange}
+              >
+                {branchOptions.map((branch) => (
+                  <MenuItem key={branch.id} value={branch.id}>
+                    {branch.name}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </Grid>
+            <Grid size={{ xs: 12, sm: 6 }}>
+              <TextField
+                fullWidth
+                select
+                id="location-select-label"
+                name="locId"
+                label="Select Location"
+                value={formData.locId}
+                onChange={handleChange}
+              >
+                {locationOptions.map((loc) => (
+                  <MenuItem key={loc.id} value={loc.id}>
+                    {loc.name}
+                  </MenuItem>
+                ))}
+              </TextField>
             </Grid>
             <Grid size={{ xs: 12 }}>
               <Typography variant="subtitle1" mt={2}>
@@ -137,40 +200,16 @@ const EmployeeForm = () => {
               </Typography>
             </Grid>
             <Grid size={{ xs: 12 }}>
-              <TextField
-                name="add1"
-                label="Address Line 1"
-                fullWidth
-                value={formData.add1}
-                onChange={handleChange}
-              />
+              <TextField name="add1" label="Address Line 1" fullWidth value={formData.add1} onChange={handleChange} />
             </Grid>
             <Grid size={{ xs: 12 }}>
-              <TextField
-                name="add2"
-                label="Address Line 2"
-                fullWidth
-                value={formData.add2}
-                onChange={handleChange}
-              />
+              <TextField name="add2" label="Address Line 2" fullWidth value={formData.add2} onChange={handleChange} />
             </Grid>
             <Grid size={{ xs: 12, sm: 6 }}>
-              <TextField
-                name="pincode"
-                label="Pincode"
-                fullWidth
-                value={formData.pincode}
-                onChange={handleChange}
-              />
+              <TextField name="pincode" label="Pincode" fullWidth value={formData.pincode} onChange={handleChange} />
             </Grid>
             <Grid size={{ xs: 12, sm: 6 }}>
-              <TextField
-                name="state"
-                label="State"
-                fullWidth
-                value={formData.state}
-                onChange={handleChange}
-              />
+              <TextField name="state" label="State" fullWidth value={formData.state} onChange={handleChange} />
             </Grid>
             <Grid size={{ xs: 12, sm: 6 }}>
               <FormControl fullWidth>
@@ -179,21 +218,19 @@ const EmployeeForm = () => {
                   labelId="city-select-label"
                   name="city"
                   label="City"
-                  value={formData.cusSCity}
+                  value={formData.city}
                   onChange={handleChange}
                 >
-                  <MenuItem value="1">Madurai</MenuItem>
-                  <MenuItem value="2">Chennai</MenuItem>
-                  <MenuItem value="3">Banglore</MenuItem>
+                  {cityOptions.length > 0 &&
+                    cityOptions?.map((city, index) => (
+                      <MenuItem key={index} value={city}>
+                        {city}
+                      </MenuItem>
+                    ))}
                 </Select>
               </FormControl>
             </Grid>
-            <Grid
-              size={12}
-              display="flex"
-              justifyContent={{ xs: "center", sm: "flex-end" }}
-              mt={2}
-            >
+            <Grid size={12} display="flex" justifyContent={{ xs: "center", sm: "flex-end" }} mt={2}>
               <Button type="submit" variant="contained">
                 Submit
               </Button>
